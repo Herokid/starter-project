f46113a1b89c53de5a272e6f36d9aabbd70ae417
Herokid <david@herokidstudio.es>
2022-03-21 17:27:41 +0100

    Refactor main app page initialiation

--- src/es6/app/app.js
+++ src/es6/app/app.js
@@ -4,16 +4,11 @@ import Pages from './pages'
 class App {
 
     constructor() {
-        const _ = this
-        _.page = document.body.dataset.page || 'default';
-        _.init();
+        const _ = this;
+        const pageReference = document.body.dataset.page;
+        const page = Pages[pageReference] || Pages['default'];
+        new page();
     }
-
-    init(page) {
-        const _ = this
-        new Pages[_.page]();
-    }
-
 };
 
 export default App;
