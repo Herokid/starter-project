5870b1048f0d7bbc76f2260cc13563ba6297f677
Herokid <david@herokidstudio.es>
2022-03-21 14:47:30 +0100

    Add main server config. Add main babel config.

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, "dist"),
+            staticOptions: {},
+            // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+            // Can be:
+            // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+            publicPath: "/",
+            // Can be:
+            // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+            serveIndex: true,
+            // Can be:
+            // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+            watch: true,
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            ]
+        }
+    }
+};
